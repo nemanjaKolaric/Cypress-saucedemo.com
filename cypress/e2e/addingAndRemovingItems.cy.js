48caf70663cf2b43599228a58b6094930e592d17
nemanjaKolaric <nkolaric1982@gmail.com>
2023-01-08 01:50:09 +0100

    oop update

--- cypress/e2e/addingAndRemovingItems.cy.js
+++ cypress/e2e/addingAndRemovingItems.cy.js
@@ -1,39 +1,41 @@
-const valid_data = require('../fixtures/valid_data.json')
+import homePage from '../pages/homePage.js'
+import itemPage from '../pages/itemPage.js'
+import checkoutPage from '../pages/checkoutPage.js'
 
 describe("Adding and removing items", () => {
     beforeEach(() => {
         cy.visit(Cypress.env("url"))
-        cy.loginForm(valid_data.standard_user, valid_data.password)
+        cy.loginForm(Cypress.env("standard_user"), Cypress.env("password"))
     })
 
     it("user add item from home page", () => {
-        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        homePage.addBackpack.click()
         cy.get('.shopping_cart_badge').should('exist')
     })
 
     it("user remove item from home page", () => {
-        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-        cy.get('#remove-sauce-labs-backpack').click()
+        homePage.addBackpack.click()
+        homePage.removeBackpack.click()
         cy.get('.shopping_cart_badge').should('not.exist')
     })
 
     it("user add item from item page", () => {
-        cy.get('.inventory_item:nth-child(1) .inventory_item_name').click()
-        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        homePage.itemTitleBackpack.click()
+        itemPage.addBackpack.click()
         cy.get('.shopping_cart_badge').should('exist')
     })
 
     it("user remove item from item page", () => {
-        cy.get('.inventory_item:nth-child(1) .inventory_item_name').click()
-        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-        cy.get('#remove-sauce-labs-backpack').click()
+        homePage.itemTitleBackpack.click()
+        itemPage.addBackpack.click()
+        itemPage.removeBackpack.click()
         cy.get('.shopping_cart_badge').should('not.exist')
     })
 
     it("user remove item from checkout page", () => {
-        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-        cy.get('.shopping_cart_link').click()
-        cy.get('#remove-sauce-labs-backpack').click()
+        homePage.addBackpack.click()
+        homePage.shoppingCartLink.click()
+        checkoutPage.removeBackpack.click()
         cy.get('.shopping_cart_badge').should('not.exist')
     })
 })
