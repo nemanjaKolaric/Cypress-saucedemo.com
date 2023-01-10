7d97f538fac51cd004ed6c76a1126c16bb975f59
nemanjaKolaric <nkolaric1982@gmail.com>
2023-01-10 21:25:13 +0100

    oop added

--- cypress/e2e/addingAndRemovingItems.cy.js
+++ cypress/e2e/addingAndRemovingItems.cy.js
@@ -10,32 +10,32 @@ describe("Adding and removing items", () => {
 
     it("user add item from home page", () => {
         homePage.addBackpack.click()
-        cy.get('.shopping_cart_badge').should('exist')
+        homePage.shoppingCartBadge.should('exist')
     })
 
     it("user remove item from home page", () => {
         homePage.addBackpack.click()
         homePage.removeBackpack.click()
-        cy.get('.shopping_cart_badge').should('not.exist')
+        homePage.shoppingCartBadge.should('not.exist')
     })
 
     it("user add item from item page", () => {
         homePage.itemTitleBackpack.click()
         itemPage.addBackpack.click()
-        cy.get('.shopping_cart_badge').should('exist')
+        homePage.shoppingCartBadge.should('exist')
     })
 
     it("user remove item from item page", () => {
         homePage.itemTitleBackpack.click()
         itemPage.addBackpack.click()
         itemPage.removeBackpack.click()
-        cy.get('.shopping_cart_badge').should('not.exist')
+        homePage.shoppingCartBadge.should('not.exist')
     })
 
     it("user remove item from checkout page", () => {
         homePage.addBackpack.click()
         homePage.shoppingCartLink.click()
         checkoutPage.removeBackpack.click()
-        cy.get('.shopping_cart_badge').should('not.exist')
+        homePage.shoppingCartBadge.should('not.exist')
     })
 })
